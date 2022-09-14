b723fd5743c36a1586ade2ab866c21751a55e865
Yuan Meng <yuanmeng961228@gmail.com>
2022-09-14 15:18:40 +0200

    service chapt3

---
+++ chapt3/chapt3_ws/src/example_service_rclpy/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'example_service_rclpy'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yuan',
+    maintainer_email='yuanmeng961228@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'service_server_02 = example_service_rclpy.service_server_02:main'
+        ],
+    },
+)
