b32cc1192606956d6016e554a29f79f35571c2c5
Yuan Meng <yuanmeng961228@gmail.com>
2022-09-19 21:34:35 +0200

    action update

---
+++ chapt4/chapt4_ws/build/robot_control_interfaces/robot_control_interfaces__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/yuan/Desktop/ROS2_Exercises/chapt4/chapt4_ws/build/robot_control_interfaces/robot_control_interfaces__py
+# Build directory: /home/yuan/Desktop/ROS2_Exercises/chapt4/chapt4_ws/build/robot_control_interfaces/robot_control_interfaces__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
