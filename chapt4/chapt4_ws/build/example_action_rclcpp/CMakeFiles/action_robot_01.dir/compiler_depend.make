527f1dc1e576ceac2944144fab20b960fa18c67b
Yuan Meng <yuanmeng961228@gmail.com>
2022-09-20 16:55:08 +0200

    action finished chapter 4

---
+++ chapt4/chapt4_ws/build/example_action_rclcpp/CMakeFiles/action_robot_01.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for action_robot_01.
+# This may be replaced when dependencies are built.
---
+++ chapt4/chapt4_ws/build/example_action_rclpy/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/yuan/Desktop/ROS2_Exercises/chapt4/chapt4_ws/install/robot_control_interfaces/share/robot_control_interfaces/package.sh"
